96429200be07398b6089a92db71d2510a4ae91dd
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-06-12 08:35:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ st.set_page_config(page_title="المنصة التعليمية قِصَّتِي 
 css_style = """
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&display=swap');
-
 html, body, [data-testid="stMarkdownContainer"] {
     font-family: 'Cairo', sans-serif !important;
     direction: RTL;
@@ -17,7 +16,7 @@ html, body, [data-testid="stMarkdownContainer"] {
 /* خلفية متدرجة جذابة */
 .stApp { 
     background: linear-gradient(135deg, #6a11cb 0%, #2575fc 100%); 
-    color: white;
+    : white;
 }
 
 /* تنسيق الأزرار الجذابة */
@@ -29,7 +28,7 @@ html, body, [data-testid="stMarkdownContainer"] {
     font-weight: 900 !important; 
     border: none !important;
     background: linear-gradient(45deg, #FF9A9E 0%, #FEC163 99%, #FEC163 100%) !important; 
-    color: #white !important;
+    : #white !important;
     box-shadow: 0px 10px 20px rgba(0,0,0,0.2) !important;
     transition: transform 0.3s ease, box-shadow 0.3s ease !important;
 }
@@ -42,7 +41,7 @@ html, body, [data-testid="stMarkdownContainer"] {
 
 /* حاوية المحتوى */
 .content-card {
-    background-color: rgba(255, 255, 255, 0.95);
+    background-color: withe(255, 255, 255, 0.95);
     padding: 40px;
     border-radius: 30px;
     color: #333;
