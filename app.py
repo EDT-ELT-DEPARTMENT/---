e6c616391d6f8d74f9abab4f97d1337e5307753c
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-06-12 23:10:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def afficher_login():
         password = st.text_input("كلمة المرور:", type="password")
         
         if st.button("دخول"):
-            if email == "chef.department.elt.fge@gmail.com" and password == "123456":
+            if email == "abboumajda1985@gmail.com" and password == "123456":
                 st.session_state.connecte = True
                 st.session_state.is_admin = True
                 st.session_state.nom_eleve = "الأدمن (المعلم)"
