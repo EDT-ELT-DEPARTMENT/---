b84882d9941a70961024718c1dcc79308d6fa069
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-06-12 09:48:39 +0100

    Fix text color property in markdown

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 # 1. إعداد الصفحة
 st.markdown("<h1 style='text-align: center; color: white;'>🎈 المَنْصَةُ التَّعْلِيمِيَّةُ قِصَّتِي دِرَاسَتِي 🎈</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: #FFD700; font-size: 20px; font-weight: bold;'>جميع هذه الدروس مطابقة تماماً للمناهج التعليمية الوطنية الجزائرية</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #FFD700; font-size: 20px; font-weight: withe;'>جميع هذه الدروس مطابقة تماماً للمناهج التعليمية الوطنية الجزائرية</p>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
 
