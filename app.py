24fc520039ba22603ef35659f8c4843d35cb5c75
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-06-14 09:42:22 +0100

    Update admin login credentials in app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def afficher_login():
         
         if st.button("دخول"):
             # منطق دخول الأدمن المباشر
-            if email == "chef.department.elt.fge@gmail.com" and password == "123456":
+            if email == "abboumajda1985" and password == "iyed2023":
                 st.session_state.connecte = True
                 st.session_state.is_admin = True
                 st.session_state.nom_eleve = "الأدمن (المعلم)"
